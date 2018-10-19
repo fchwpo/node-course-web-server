85401d9ebb560c6ba9dfec3563bd73654dc46f9d
Shubham Vishwakarma <tcet.shubham@gmail.com>
2018-10-20 03:25:04 +0530

    "Commit for Heroku"

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const hbs = require('hbs');
 const fs = require('fs');
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
@@ -56,6 +58,6 @@ app.get('/bad', (request, response) => {
   });
 });
 
-app.listen(3000, ()=> {
-  console.log('Server is up and listening on port 3000');
+app.listen(port, ()=> {
+  console.log(`Server is up and listening on port ${port}`);
 });
